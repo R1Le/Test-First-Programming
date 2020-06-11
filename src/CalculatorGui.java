b0498512df013f3f1d20bcc029ab17ed4efd5e55
R1Le <petar.r016@gmail.com>
2020-06-11 15:26:05 +0200

    First phase: Gui for calculator and tests for getting single-digit numbers from it.

---
+++ src/CalculatorGui.java
@@ -0,0 +1,188 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.Point;
+import javax.swing.JTextField;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import java.awt.Font;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class CalculatorGui extends JFrame {
+   
+    private static final long serialVersionUID = 1L;
+    public JTextField result;
+    
+    JButton number0;
+    JButton number1;
+    JButton number2;
+    JButton number3;
+    JButton number4;
+    JButton number5;
+    JButton number6;
+    JButton number7;
+    JButton number8;
+    JButton number9;
+    
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    CalculatorGui frame = new CalculatorGui();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+    
+    /**
+     * Create the frame.
+     */
+    public CalculatorGui() {
+        setTitle("Calculator");
+        setType(Type.UTILITY);
+        setLocation(new Point(500, 250));
+        setResizable(false);
+        setBounds(100, 100, 450, 550);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        result = new JTextField();
+        result.setBackground(Color.WHITE);
+        result.setEditable(false);
+        result.setHorizontalAlignment(SwingConstants.TRAILING);
+        result.setFont(new Font("Tahoma", Font.BOLD, 23));
+        result.setColumns(10);
+        
+        number0 = new JButton("0");
+        number0.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                result.setText("0");
+            }
+        });
+        
+        number1 = new JButton("1");
+        number1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                result.setText("1");
+            }
+        });
+        
+        number2 = new JButton("2");
+        number2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                result.setText("2");
+            }
+        });
+        
+        number3 = new JButton("3");
+        number3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                result.setText("3");
+            }
+        });
+        
+        number4 = new JButton("4");
+        number4.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                result.setText("4");
+            }
+        });
+        
+        number5 = new JButton("5");
+        number5.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                result.setText("5");
+            }
+        });
+        
+        number6 = new JButton("6");
+        number6.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                result.setText("6");
+            }
+        });
+        
+        number7 = new JButton("7");
+        number7.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                result.setText("7");
+            }
+        });
+        
+        number8 = new JButton("8");
+        number8.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                result.setText("8");
+            }
+        });
+        
+        number9 = new JButton("9");
+        number9.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                result.setText("9");
+            }
+        });
+        
+        GroupLayout groupLayout = new GroupLayout(getContentPane());
+        groupLayout.setHorizontalGroup(
+            groupLayout.createParallelGroup(Alignment.LEADING)
+                .addGroup(groupLayout.createSequentialGroup()
+                    .addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+                        .addGroup(groupLayout.createSequentialGroup()
+                            .addContainerGap()
+                            .addComponent(result, GroupLayout.DEFAULT_SIZE, 414, Short.MAX_VALUE))
+                        .addGroup(groupLayout.createSequentialGroup()
+                            .addGap(193)
+                            .addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+                                .addComponent(number8)
+                                .addComponent(number9)
+                                .addComponent(number7)
+                                .addComponent(number6)
+                                .addComponent(number5)
+                                .addComponent(number4)
+                                .addComponent(number3)
+                                .addComponent(number2)
+                                .addComponent(number1)
+                                .addComponent(number0))))
+                    .addContainerGap())
+        );
+        groupLayout.setVerticalGroup(
+            groupLayout.createParallelGroup(Alignment.LEADING)
+                .addGroup(groupLayout.createSequentialGroup()
+                    .addContainerGap()
+                    .addComponent(result, GroupLayout.PREFERRED_SIZE, 70, GroupLayout.PREFERRED_SIZE)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addComponent(number9)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addComponent(number8)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addComponent(number7)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addComponent(number6)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addComponent(number5)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addComponent(number4)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addComponent(number3)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addComponent(number2)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addComponent(number1)
+                    .addPreferredGap(ComponentPlacement.RELATED)
+                    .addComponent(number0)
+                    .addContainerGap(44, Short.MAX_VALUE))
+        );
+        getContentPane().setLayout(groupLayout);
+    }
+}
---
+++ test/Phase1.java
@@ -0,0 +1,83 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class Phase1 {
+    
+    private static CalculatorGui calculator;
+    
+    @BeforeAll
+    public static void createCalculator() {
+        calculator = new CalculatorGui();
+    }
+    
+    @Test
+    void getNumberZero() {
+        calculator.number0.doClick();
+        assertEquals(0, Integer.parseInt(calculator.result.getText()));
+    }
+    
+    @Test
+    void getNumberOne() {
+        calculator.number1.doClick();
+        assertEquals(1, Integer.parseInt(calculator.result.getText()));
+    }
+    
+    @Test
+    void getNumberTwo() {
+        calculator.number2.doClick();
+        assertEquals(2, Integer.parseInt(calculator.result.getText()));
+    }
+    
+    @Test
+    void getNumberThree() {
+        calculator.number3.doClick();
+        assertEquals(3, Integer.parseInt(calculator.result.getText()));
+    }
+    
+    @Test
+    void getNumberFour() {
+        calculator.number4.doClick();
+        assertEquals(4, Integer.parseInt(calculator.result.getText()));
+    }
+    
+    @Test
+    void getNumberFive() {
+        calculator.number5.doClick();
+        assertEquals(5, Integer.parseInt(calculator.result.getText()));
+    }
+    
+    @Test
+    void getNumberSix() {
+        calculator.number6.doClick();
+        assertEquals(6, Integer.parseInt(calculator.result.getText()));
+    }
+    
+    @Test
+    void getNumberSeven() {
+        calculator.number7.doClick();
+        assertEquals(7, Integer.parseInt(calculator.result.getText()));
+    }
+    
+    @Test
+    void getNumberEight() {
+        calculator.number8.doClick();
+        assertEquals(8, Integer.parseInt(calculator.result.getText()));
+    }
+    
+    @Test
+    void getNumberNine() {
+        calculator.number9.doClick();
+        assertEquals(9, Integer.parseInt(calculator.result.getText()));
+    }
+    
+    //Multi-digit numbers are not implemented in first phase.
+    @Test
+    void getMultiNumber() {
+        calculator.number9.doClick();
+        calculator.number8.doClick();
+        assertEquals(98, Integer.parseInt(calculator.result.getText()));
+    }
+    
+}
